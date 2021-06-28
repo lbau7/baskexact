15754e99f4d950ae93fa6623af2399ce50295218
lbau7 <l.baumann@aon.at>
2021-06-28 11:52:59 +0200

    remove unnecessary Rcpp functions

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,45 +6,6 @@
 
 using namespace Rcpp;
 
-// get_res_fin
-NumericVector get_res_fin(const NumericVector& prob, const NumericVector& res_int, const double& lambda);
-RcppExport SEXP _baskexact_get_res_fin(SEXP probSEXP, SEXP res_intSEXP, SEXP lambdaSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const NumericVector& >::type prob(probSEXP);
-    Rcpp::traits::input_parameter< const NumericVector& >::type res_int(res_intSEXP);
-    Rcpp::traits::input_parameter< const double& >::type lambda(lambdaSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_res_fin(prob, res_int, lambda));
-    return rcpp_result_gen;
-END_RCPP
-}
-// get_n_vec
-NumericVector get_n_vec(const int& n, const int& n1, const NumericVector& res_int);
-RcppExport SEXP _baskexact_get_n_vec(SEXP nSEXP, SEXP n1SEXP, SEXP res_intSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const int& >::type n(nSEXP);
-    Rcpp::traits::input_parameter< const int& >::type n1(n1SEXP);
-    Rcpp::traits::input_parameter< const NumericVector& >::type res_int(res_intSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_n_vec(n, n1, res_int));
-    return rcpp_result_gen;
-END_RCPP
-}
-// get_r_temp
-NumericVector get_r_temp(const int& n1, const NumericVector& r, const NumericVector& res_int);
-RcppExport SEXP _baskexact_get_r_temp(SEXP n1SEXP, SEXP rSEXP, SEXP res_intSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const int& >::type n1(n1SEXP);
-    Rcpp::traits::input_parameter< const NumericVector& >::type r(rSEXP);
-    Rcpp::traits::input_parameter< const NumericVector& >::type res_int(res_intSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_r_temp(n1, r, res_int));
-    return rcpp_result_gen;
-END_RCPP
-}
 // weight_beta
 arma::mat weight_beta(const int& k, const arma::vec& weights, const arma::mat& shape);
 RcppExport SEXP _baskexact_weight_beta(SEXP kSEXP, SEXP weightsSEXP, SEXP shapeSEXP) {
@@ -60,9 +21,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_baskexact_get_res_fin", (DL_FUNC) &_baskexact_get_res_fin, 3},
-    {"_baskexact_get_n_vec", (DL_FUNC) &_baskexact_get_n_vec, 3},
-    {"_baskexact_get_r_temp", (DL_FUNC) &_baskexact_get_r_temp, 3},
     {"_baskexact_weight_beta", (DL_FUNC) &_baskexact_weight_beta, 3},
     {NULL, NULL, 0}
 };
