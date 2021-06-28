164fc6e1766fed4ac0bb58b2cc2f1b24eca3bab6
lbau7 <l.baumann@aon.at>
2021-06-28 14:18:38 +0200

    add reject_single_loop as validation-function

--- R/validate.R
+++ R/validate.R
@@ -2,18 +2,54 @@
 # validate the results of the main functions, by using other (mostly
 # slower but easier to follow) algorithms to calculate the same values.
 
-reject_prob_ew_slow <- function(design, n, lambda, weight_mat,
-                                prob = c("toer", "pwr")) {
+# Loop-based calculation of the rejection probabilities of a single-stage
+# basket design with 3 baskets
+reject_single_loop <- function(design, n, lambda, epsilon, tau,
+                               logbase = exp(1), prune = FALSE,
+                               prob = c("toer", "pwr")) {
+  if (all(design@theta1 != design@theta0)) {
+    design@theta1 <- rep(design@theta0, design@k)
+  }
+
   targ <- get_targ(design = design, prob = prob)
-  events <- arrangements::permutations(0:n, k = design@k, replace = TRUE)
+  rej_ew <- 0
+  rej_group <- c(0, 0, 0)
+  weights <- get_weights(design = design, n = n, epsilon = epsilon, tau = tau,
+    logbase = logbase)
 
-  fun <- function(x) bskt_final(design = design, n = n, lambda = lambda, r = x,
-    weight_mat = weight_mat)
-  res <- t(apply(events, 1, fun))
+  if (prune) {
+    crit_pool <- get_crit_pool(design = design, n = n, lambda = lambda)
+    weights <- prune_weights(weight_mat = weights, cut = crit_pool)
+  }
 
-  eff_vec <- apply(res, 1, function(x) any(x[targ] == 1))
-  events_eff <- events[eff_vec, ]
-  probs_eff <- apply(events_eff, 1,
-    function(x) get_prob(n = n, r = x, theta = design@theta1))
-  sum(probs_eff)
+  for (i1 in 0:n) {
+    for (i2 in 0:n) {
+      for (i3 in 0:n) {
+        events <- c(i1, i2, i3)
+        res <- bskt_final(design = design, n = n, lambda = lambda, r = events,
+          weight_mat = weights)
+
+        if (any(res[targ] == 1)) {
+          prob_temp <- get_prob(n = n, r = events, theta = design@theta1)
+          rej_ew <- rej_ew + prob_temp
+          rej_group[which(res[targ] == 1)] <- rej_group[which(res[targ] == 1)] +
+            prob_temp
+        }
+      }
+    }
+  }
+
+  if (prob == "toer") {
+    list(
+      rejection_probabilities = rej_group,
+      fwer = rej_ew
+    )
+  } else {
+    list(
+      rejection_probabilities = rej_group,
+      ewp = rej_ew
+    )
+  }
 }
+
+
