606e5cfe23be8c02a7b877134207e2e04a9eb40c
lbau7 <l.baumann@aon.at>
2021-07-05 16:48:54 +0200

    add tests

--- tests/testthat/test-adjust_lambda.R
+++ tests/testthat/test-adjust_lambda.R
@@ -15,10 +15,27 @@ test_that("adjust_lambda works", {
   adj_res2 <- adjust_lambda(design = design, alpha = 0.025, n = 15,
     epsilon = 1, tau = 0, logbase = 2, prune = TRUE, prec_digits = 4)
   lambda_high2 <- adj_res2$lambda - 0.0001
-  toer_adj2 = toer(design = design, n = 15, lambda = adj_res2$lambda,
+  toer_adj2 <- toer(design = design, n = 15, lambda = adj_res2$lambda,
     epsilon = 1, tau = 0, logbase = 2, prune = TRUE, results = "fwer")
   toer_high2 <- toer(design = design, n = 15, lambda = lambda_high2,
     epsilon = 1, tau = 0, logbase = 2, prune = TRUE, results = "fwer")
+
+  # Cases with additional steps after uniroot
+  adj_res3 <- adjust_lambda(design = design, alpha = 0.025, n = 15,
+    epsilon = 4, tau = 0, logbase = 2, prune = FALSE, prec_digits = 4)
+  lambda_high3 <- adj_res3$lambda - 0.0001
+  toer_adj3 <- toer(design = design, n = 15, lambda = adj_res3$lambda,
+    epsilon = 4, tau = 0, logbase = 2, prune = FALSE, results = "fwer")
+  toer_high3 <- toer(design = design, n = 15, lambda = lambda_high3,
+    epsilon = 4, tau = 0, logbase = 2, prune = FALSE, results = "fwer")
+
+  adj_res4 <- adjust_lambda(design = design, alpha = 0.025, n = 15,
+    epsilon = 1, tau = 0.2, logbase = 2, prune = FALSE, prec_digits = 4)
+  lambda_high4 <- adj_res4$lambda - 0.0001
+  toer_adj4 <- toer(design = design, n = 15, lambda = adj_res4$lambda,
+    epsilon = 1, tau = 0.2, logbase = 2, prune = FALSE)
+  toer_high4 <- toer(design = design, n = 15, lambda = lambda_high4,
+    epsilon = 1, tau = 0.2, logbase = 2, prune = FALSE)
 
   expect_equal(adj_res1$toer, toer_adj1)
   expect_lte(adj_res1$toer, 0.025)
@@ -26,4 +43,10 @@ test_that("adjust_lambda works", {
   expect_equal(adj_res2$toer, toer_adj2)
   expect_lte(adj_res2$toer, 0.025)
   expect_gt(toer_high2, 0.025)
+  expect_equal(adj_res3$toer, toer_adj3)
+  expect_lte(adj_res3$toer, 0.025)
+  expect_gt(toer_high3, 0.025)
+  expect_equal(adj_res3$toer, toer_adj3)
+  expect_lte(adj_res3$toer, 0.025)
+  expect_gt(toer_high3, 0.025)
 })
