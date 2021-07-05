156ff5eb5ab4a67fa1acaeeec4ac45db3e7ba473
lbau7 <l.baumann@aon.at>
2021-07-05 09:22:07 +0200

    add option prune to pow

--- R/pow.R
+++ R/pow.R
@@ -1,6 +1,6 @@
 #' @describeIn pow Power for a single-stage design.
 setMethod("pow", "OneStageBasket",
-  function(design, n, lambda, epsilon, tau, logbase = 2,
+  function(design, n, lambda, epsilon, tau, logbase = 2, prune = FALSE,
            results = c("ewp", "group"), ...) {
     if (length(n) != 1) stop("n must have length 1")
     if (lambda <= 0 | lambda >= 1) stop("lambda must be between 0 and 1")
@@ -11,6 +11,11 @@ setMethod("pow", "OneStageBasket",
     results <- match.arg(results)
     weight_mat <- get_weights(design = design, n = n, epsilon = epsilon,
       tau = tau, logbase = logbase)
+
+    if (prune) {
+      crit_pool <- get_crit_pool(design = design, n = n, lambda = lambda)
+      weight_mat <- prune_weights(weight_mat = weight_mat, cut = crit_pool)
+    }
 
     if (results == "ewp") {
       reject_prob_ew(design = design, n = n, lambda = lambda,
