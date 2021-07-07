256cea8e500bbcd23c88688ce2f986b8afe8c45b
lbau7 <l.baumann@aon.at>
2021-07-07 16:39:26 +0200

    add tests

--- tests/testthat/test-monotonicity.R
+++ tests/testthat/test-monotonicity.R
@@ -33,11 +33,11 @@ test_that("check_mon_within works", {
   expect_false(any(res4 != cummax(res4)))
   expect_false(any(res5 != cummax(res5)))
 
-  # Check condition with low details
-  res_lowdet1 <- check_mon_within(design = design1, n = 24, lambda = 0.99,
+  # Check condition with no details
+  res_nodet1 <- check_mon_within(design = design1, n = 24, lambda = 0.99,
     epsilon = 0.5, tau = 0, logbase = 2, prune = FALSE, details = FALSE)
 
-  expect_false(res_lowdet1)
+  expect_false(res_nodet1)
 
   # Compare with mon_within_loop
   res_loop1 <- mon_within_loop(design = design1, n = 24, lambda = 0.99,
@@ -81,11 +81,11 @@ test_that("check_mon_within works", {
 
   expect_true(all(resall == r$Results))
 
-  # Check condition with low details
-  res_lowdet2 <- check_mon_within(design = design1, n = 24, lambda = 0.99,
+  # Check condition with no details
+  res_nodet2 <- check_mon_within(design = design1, n = 24, lambda = 0.99,
     epsilon = 7, tau = 0, logbase = 2, prune = TRUE, details = FALSE)
 
-  expect_false(res_lowdet2)
+  expect_false(res_nodet2)
 
   # Compare with mon_within_loop
   res_loop2 <- mon_within_loop(design = design1, n = 24, lambda = 0.99,
@@ -125,38 +125,80 @@ test_that("errors in check_mon_within work", {
 })
 
 test_that("check_mon_between works", {
-  # Without Pruning
-  design <- setupOneStageBasket(k = 4, shape1 = 1, shape2 = 1, theta0 = 0.2,
-    theta1 = rep(0.5, 4))
-  ev1 <- check_mon_between(design = design, n = 15, lambda = 0.99,
-    epsilon = 2, tau = 0, logbase = 2, prune = FALSE, details = TRUE)[[1]]
+  ## Without Pruning
+  design <- setupOneStageBasket(k = 3, shape1 = 1, shape2 = 1, theta0 = 0.2,
+    theta1 = rep(0.5, 3))
 
-  weights1 <- get_weights(design = design, n = 15, epsilon = 2, tau = 0,
+  ev <- check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = 2, prune = FALSE, details = TRUE)
+  ev_viol <- t(sapply(ev, function(x) x$Events[1, ]))
+
+  # Check results of first violated outcome
+  ev1 <- ev[[1]]
+  weights1 <- get_weights(design = design, n = 20, epsilon = 2, tau = 0,
     logbase = 2)
-  res1 <- bskt_final(design = design, n = 15, lambda = 0.99,
+  res1 <- bskt_final(design = design, n = 20, lambda = 0.99,
     r = ev1$Events[1, ], weight_mat = weights1)
-  res2 <- bskt_final(design = design, n = 15, lambda = 0.99,
+  res2 <- bskt_final(design = design, n = 20, lambda = 0.99,
     r = ev1$Events[2, ], weight_mat = weights1)
-
-  res_nodet1 <- check_mon_between(design = design, n = 15, lambda = 0.99,
-    epsilon = 2, tau = 0, logbase = 2, prune = FALSE, details = FALSE)
-
-  # With Pruning
-  res_nodet2 <- check_mon_between(design = design, n = 15, lambda = 0.99,
-    epsilon = 2, tau = 0, logbase = 2, prune = TRUE, details = FALSE)
-
-  crit_pool <- get_crit_pool(design = design, n = 15, lambda = 0.99)
-  weights2 <- prune_weights(weights1, cut = crit_pool)
-  res3 <- bskt_final(design = design, n = 15, lambda = 0.99,
-    r = ev1$Events[1, ], weight_mat = weights2)
-  res4 <- bskt_final(design = design, n = 15, lambda = 0.99,
-    r = ev1$Events[2, ], weight_mat = weights2)
+  res3 <- bskt_final(design = design, n = 20, lambda = 0.99,
+    r = ev1$Events[3, ], weight_mat = weights1)
 
   expect_equal(res1, ev1$Results[1, ])
   expect_true(any(res1 == 1))
   expect_equal(res2, ev1$Results[2, ])
   expect_true(all(res2 == 0))
+  expect_equal(res3, ev1$Results[3, ])
+  expect_true(all(res3 == 0))
+
+  # Check condition with no details
+  res_nodet1 <- check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = 2, prune = FALSE, details = FALSE)
+
   expect_false(res_nodet1)
-  expect_equal(res3, res4)
+
+  # Compare with mon_between_slow
+  res_slow1 <- mon_between_slow(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = 2, prune = FALSE)
+
+  expect_equal(ev_viol, res_slow1)
+
+  ## With Pruning
+  res_nodet2 <- check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = 2, prune = TRUE, details = FALSE)
+
   expect_true(res_nodet2)
+
+  # Check violating outcomes from no-prune analysis
+  crit_pool <- get_crit_pool(design = design, n = 20, lambda = 0.99)
+  weights2 <- prune_weights(weights1, cut = crit_pool)
+  res3 <- bskt_final(design = design, n = 20, lambda = 0.99,
+    r = ev1$Events[1, ], weight_mat = weights2)
+  res4 <- bskt_final(design = design, n = 20, lambda = 0.99,
+    r = ev1$Events[2, ], weight_mat = weights2)
+
+  expect_true(all(res3 == 0))
+  expect_true(all(res4 == 0))
+
+  # Compare with mon_between_slow
+  res_slow2 <- mon_between_slow(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = 2, prune = TRUE)
+
+  expect_true(res_nodet2, res_slow2)
+})
+
+test_that("errors in check_mon_within work", {
+  design <- setupOneStageBasket(k = 3, shape1 = 1, shape2 = 1, theta0 = 0.2,
+    theta1 = c(0.2, 0.5, 0.5))
+
+  expect_error(check_mon_between(design = design, n = c(10, 15, 20),
+    lambda = 0.99, epsilon = 2, tau = 0, logbase = 2, results = "fwer"))
+  expect_error(check_mon_between(design = design, n = 20, lambda = -1,
+    epsilon = 2, tau = 0, logbase = 2, results = "fwer"))
+  expect_error(check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = -2, tau = 0, logbase = 2, results = "fwer"))
+  expect_error(check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 1, logbase = 2, results = "fwer"))
+  expect_error(check_mon_between(design = design, n = 20, lambda = 0.99,
+    epsilon = 2, tau = 0, logbase = -2, results = "fwer"))
 })
