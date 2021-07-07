ecca0d8717799429d8a0397c8b163c08a9bfd331
lbau7 <l.baumann@aon.at>
2021-07-07 15:44:52 +0200

    add tests

--- tests/testthat/test-borrowing.R
+++ tests/testthat/test-borrowing.R
@@ -1,5 +1,5 @@
 test_that("get_weight works", {
-  # Reproduced from Fujikawa et al. 2020, Supplement R code
+  # Reproduced from Fujikawa et al., 2020, Supplement R code
   design <- setupOneStageBasket(k = 3, shape1 = 1, shape2 = 1, theta0 = 0.2,
     theta1 = c(0.5, 0.5, 0.2))
   weight_mat <- get_weights(design = design, n = 15, epsilon = 2, tau = 0,
@@ -10,4 +10,21 @@ test_that("get_weight works", {
   weights_exp <- c(0.3206983, 0.7493639, 0.6509846)
 
   expect_equal(weights, weights_exp, tolerance = 1e-7)
+})
+
+test_that("beta_borrow works", {
+  # Reproduced from Fujikawa et al., 2020, Supplement R Code
+  design <- setupOneStageBasket(k = 3, shape1 = 1, shape2 = 1, theta0 = 0.2,
+    theta1 = c(0.2, 0.2, 0.2))
+  weight_mat <- get_weights(design = design, n = 24, epsilon = 2, tau = 0.5,
+    logbase = exp(1))
+  r <- c(7, 2, 5)
+  shape_post <- matrix(c(design@shape1 + r, design@shape2 + 24 - r),
+    byrow = TRUE, ncol = design@k)
+  shape_borrow <- beta_borrow(k = 3, r = r, weight_mat = weight_mat,
+    shape = shape_post)
+  shape_expect <- matrix(c(12.9215409, 34.4051363, 6.33262523, 34.1087508,
+    14.2283671, 47.5396861), nrow = 2)
+
+  expect_equal(shape_borrow, shape_expect, tolerance = 1e-7)
 })
