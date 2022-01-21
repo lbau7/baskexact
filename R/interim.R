c5b8281919a2e6f8612683b3d3a4d9afc3489607
lbau7 <l.baumann@aon.at>
2022-01-21 14:56:35 +0100

    add interim based on posterior probabilities

--- R/interim.R
+++ R/interim.R
@@ -1,7 +1,7 @@
 #' @include class.R
 NULL
 
-#' Interim Analysis Based on the Posterior Predictive Probability
+#' Interim analysis based on the posterior predictive probability
 setGeneric("interim_postpred",
   function(design, ...) standardGeneric("interim_postpred")
 )
@@ -12,11 +12,24 @@ setMethod("interim_postpred", "TwoStageBasket",
   function(design, n, n1, r1, lambda, weight_mat, prob_futstop = 0.1,
            prob_effstop = 0.9, ...) {
     crit <- get_crit(design = design, n = n, lambda = lambda)
-    shape_post <- matrix(c(design@shape1 + r1, design@shape2 + n1 - r1),
-      byrow = TRUE, ncol = design@k)
-    shape_borrow <- beta_borrow(k = design@k, r = r1, weight_mat = weight_mat,
-      shape = shape_post)
+    shape_borrow <- beta_borrow(weight_mat = weight_mat, design = design,
+      n = n1, r = r1)
     pred_prob <- post_pred(n = n, n1 = n1, r1 = r1, shape = shape_borrow,
       crit = crit)
     ifelse(pred_prob < prob_futstop, -1, ifelse(pred_prob > prob_effstop, 1, 0))
+  })
+
+#' Interim analysis based on the posterior probability
+setGeneric("interim_posterior",
+  function(design, ...) standardGeneric("interim_posterior")
+)
+
+#' @describeIn interim_posterior Interim analysis based on the posterior
+#'   probabilty for two-stage basket designs.
+setMethod("interim_posterior", "TwoStageBasket",
+  function(design, n1, r1, weight_mat, prob_futstop, prob_effstop, ...) {
+    shape_borrow <- beta_borrow(weight_mat = weight_mat, design = design,
+      n = n1, r = r)
+    post_prob <- post_beta(shape = shape_borrow, theta0 = design@theta0)
+    ifelse(post_prob < prob_futstop, -1, ifelse(post_prob > prob_effstop, 1, 0))
   })
