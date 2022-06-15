4f11a919dae5636ef85059fa32c00c5378593d4f
lbau7 <l.baumann@aon.at>
2022-06-15 17:05:47 +0200

    extend plot_weights

--- R/plot.R
+++ R/plot.R
@@ -48,8 +48,37 @@ plot_weights <- function(n, r1, weight_fun, weight_params = list()) {
       ggplot2::ylim(0, 1) +
       ggplot2::theme_bw() +
       ggplot2::scale_color_discrete(name = names(index))
+  } else if (sum(lapply(weight_params, length) > 1) == 2) {
+    index <- which(lapply(weight_params, length) > 1)
+    param_grid <- expand.grid(weight_params)
+    plotdf <- data.frame()
+
+    for (i in 1:nrow(param_grid)) {
+      weight_mat <- do.call(weight_fun, args = c(as.list(param_grid[i, ]),
+        design = design, n = n))
+      weight_mat_temp <- cbind(
+        rep(param_grid[i, index[1]], n + 1),
+        rep(param_grid[i, index[2]], n + 1),
+        0:n,
+        weight_mat[r1 + 1, ]
+      )
+      plotdf <- rbind(plotdf, weight_mat_temp)
+    }
+    names(plotdf) <- c("param1", "param2", "r", "weight")
+    plotdf$param1 <- factor(plotdf$param1)
+    plotdf$param2 <- factor(plotdf$param2)
+    ggplot2::ggplot(plotdf, ggplot2::aes(x = r, y = weight)) +
+      ggplot2::geom_line(ggplot2::aes(group = param1, colour = param1)) +
+      ggplot2::facet_grid(
+        cols = vars(param2),
+        labeller = as_labeller(function(x) paste(names(index)[2], "=", x))
+      ) +
+      ggplot2::xlim(0, n) +
+      ggplot2::ylim(0, 1) +
+      ggplot2::theme_bw() +
+      ggplot2::scale_color_discrete(name = names(index)[1])
   } else {
-    stop("only one weight parameter can have length > 1")
+    stop("at most two weight parameter can have length > 1")
   }
 }
 
