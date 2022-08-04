14df1ecb7ee28c54b25c06a05c7a58f22fabc66c
lbau7 <l.baumann@aon.at>
2022-08-04 13:49:30 +0200

    add tests

---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,26 @@
+test_that("plot_weights_work", {
+  plot1 <- plot_weights(n = 20, r1 = 10,
+    weight_fun = weights_cpp,
+    weight_params = list(a = 1, b = 1)
+  )
+
+  expect_s3_class(plot1, "ggplot")
+  expect_true(all(plot1$data$r == 0:20))
+
+  plot2 <- plot_weights(n = 20, r1 = 10,
+    weight_fun = weights_cpp,
+    weight_params = list(a = c(1, 2, 3), b = 1)
+  )
+
+  expect_s3_class(plot2, "ggplot")
+  expect_true(all(unique(plot2$param) %in% 1:3))
+
+  plot3 <- plot_weights(n = 20, r1 = 10,
+    weight_fun = weights_cpp,
+    weight_params = list(a = c(1, 2, 3), b = c(1, 2, 3))
+  )
+
+  expect_s3_class(plot3, "ggplot")
+  expect_true(all(unique(plot3$param1) %in% 1:3))
+  expect_true(all(unique(plot3$param2) %in% 1:3))
+})
