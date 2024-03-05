2d48e389e484aebd2bc86d72d9c7fb05483f3e81
lbau7 <l.baumann@aon.at>
2024-03-05 16:17:17 +0100

    add two-stage method for opt_design

--- R/opt_design.R
+++ R/opt_design.R
@@ -101,6 +101,86 @@ setMethod("opt_design", "OneStageBasket",
     }
   })
 
+#' @describeIn opt_design Optimize a two-stage basket design.
+#'
+#' @template design
+#' @template n
+#' @template n1
+#' @template alpha
+#' @template interim
+#' @template weights
+#' @template globalweights
+#' @template scenarios
+#' @template prec_digits
+#' @template dotdotdot
+setMethod("opt_design", "TwoStageBasket",
+  function(design, n, n1, alpha, interim_fun, interim_params, weight_fun,
+           weight_params = list(), globalweight_fun = NULL,
+           globalweight_params = list(), scenarios, prec_digits, ...) {
+    all_params <- c(weight_params, globalweight_params, interim_params)
+    grid <- expand.grid(all_params)
+    if (length(all_params) == 0) {
+      lgrid <- 1
+    } else {
+      lgrid <- nrow(grid)
+    }
+
+    l1 <- length(weight_params)
+    l2 <- length(globalweight_params)
+    l3 <- length(interim_params)
+    lambdas <- numeric(lgrid)
+    p <- progressr::progressor(steps = lgrid)
+
+    ecd_res <- foreach(i = 1:lgrid, .combine = 'rbind',
+      .options.future = list(seed = TRUE)) %dofuture% {
+        res_loop <- numeric(ncol(scenarios) + 1)
+        if (l1 >= 1) {
+          ploop1 <- as.list(grid[i, 1:l1, drop = FALSE])
+        } else {
+          ploop1 <- list()
+        }
+        if (l2 >= 1) {
+          ploop2 <- as.list(grid[i, (l1 + 1):(l1 + l2), drop = FALSE])
+        } else {
+          ploop2 <- list()
+        }
+        if (l3 >= 1) {
+          ploop3 <- as.list(grid[i, (l1 + l2 + 1):(l1 + l2 + l3), drop = FALSE])
+        } else {
+          ploop3 <- list()
+        }
+
+        l <- do.call(adjust_lambda, args = c(design = list(design), n = n,
+          n1 = n1, p1 = NULL, alpha = alpha, interim_fun = interim_fun,
+          interim_params = list(ploop3), weight_fun = weight_fun,
+          weight_params = list(ploop1), globalweight_fun = globalweight_fun,
+          globalweight_params = list(ploop2), prec_digits = prec_digits, ...))
+        res_loop[1] <- l$lambda
+
+        for (j in 1:ncol(scenarios)) {
+          res_loop[j + 1] <- do.call(ecd, args = c(design = list(design),
+            p1 = list(scenarios[, j]), n = n, n1 = n1, lambda = l$lambda,
+            interim_fun = interim_fun, interim_params = list(ploop3),
+            weight_fun = weight_fun, weight_params = list(ploop1),
+            globalweight_fun = globalweight_fun,
+            globalweight_params = list(ploop2), ...))
+        }
+        p()
+        res_loop
+      }
+    if (lgrid == 1) {
+      names(ecd_res) <- c("Lambda", colnames(scenarios))
+      ecd_res <- c(ecd_res, "Mean_ECD" = mean(ecd_res[-1]))
+      as.data.frame(t(ecd_res))
+    } else {
+      colnames(ecd_res) <- c("Lambda", colnames(scenarios))
+      ecd_res <- cbind(grid, ecd_res, "Mean_ECD" = rowMeans(ecd_res[, -1]))
+      ecd_res <- ecd_res[order(ecd_res[, ncol(ecd_res)], decreasing = TRUE), ]
+      rownames(ecd_res) <- NULL
+      ecd_res
+    }
+  })
+
 #' Create a Scenario Matrix
 #'
 #' Creates a default scenario matrix.
--- tests/testthat/test-opt_design.R
+++ tests/testthat/test-opt_design.R
@@ -1,4 +1,5 @@
 test_that("opt_design works", {
+  # Single-stage design
   design <- setupOneStageBasket(k = 3, p0 = 0.2)
   optres1 <- opt_design(design = design, n = 15, alpha = 0.05,
     weight_fun = weights_fujikawa, weight_params = list(epsilon = c(1, 2),
@@ -65,4 +66,60 @@ test_that("opt_design works", {
   expect_equal(as.numeric(optres2[4]), ecdres32)
   expect_equal(as.numeric(optres2[5]), ecdres42)
   expect_equal(as.numeric(optres2[6]), mean(c(ecdres12, ecdres22, ecdres32, ecdres42)))
+
+  # Two-stage design
+  design2 <- setupTwoStageBasket(k = 3, p0 = 0.2)
+  optres_2stage <- opt_design(
+    design = design2,
+    n = 7,
+    n1 = 3,
+    alpha = 0.1,
+    interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = c(0.1, 0.2), prob_effstop = 0.99),
+    weight_fun = weights_cpp,
+    weight_params = list(a = c(1, 2), b = 1),
+    scenarios = get_scenarios(design, 0.5),
+    prec_digits = 3
+  )
+
+  lambdres_2stage <- adjust_lambda(design = design2, alpha = 0.1, n = 7, n1 = 3,
+    interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = opt_2stage[1, "prob_futstop"],
+      prob_effstop = opt_2stage[1, "prob_effstop"]),
+    weight_fun = weights_cpp,
+    weight_params = list(a = opt_2stage[1, "a"], b = opt_2stage[1, "b"]),
+    prec_digits = 3)
+  ecdres1_2stage <- ecd(design = design2, n = 7, n1 = 3,
+    lambda = lambdres_2stage$lambda, interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = opt_2stage[1, "prob_futstop"],
+      prob_effstop = opt_2stage[1, "prob_effstop"]),
+    weight_fun = weights_cpp,
+    weight_params = list(a = opt_2stage[1, "a"], b = opt_2stage[1, "b"]))
+  ecdres2_2stage <- ecd(design = design2, n = 7, n1 = 3, p1 = c(0.2, 0.2, 0.5),
+    lambda = lambdres_2stage$lambda, interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = opt_2stage[1, "prob_futstop"],
+      prob_effstop = opt_2stage[1, "prob_effstop"]),
+    weight_fun = weights_cpp,
+    weight_params = list(a = opt_2stage[1, "a"], b = opt_2stage[1, "b"]))
+  ecdres3_2stage <- ecd(design = design2, n = 7, n1 = 3, p1 = c(0.2, 0.5, 0.5),
+    lambda = lambdres_2stage$lambda, interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = opt_2stage[1, "prob_futstop"],
+      prob_effstop = opt_2stage[1, "prob_effstop"]),
+    weight_fun = weights_cpp,
+    weight_params = list(a = opt_2stage[1, "a"], b = opt_2stage[1, "b"]))
+  ecdres4_2stage <- ecd(design = design2, n = 7, n1 = 3, p1 = c(0.5, 0.5, 0.5),
+    lambda = lambdres_2stage$lambda, interim_fun = interim_posterior,
+    interim_params = list(prob_futstop = opt_2stage[1, "prob_futstop"],
+      prob_effstop = opt_2stage[1, "prob_effstop"]),
+    weight_fun = weights_cpp,
+    weight_params = list(a = opt_2stage[1, "a"], b = opt_2stage[1, "b"]))
+  meanecd_2stage <- mean(c(ecdres1_2stage, ecdres2_2stage, ecdres3_2stage,
+    ecdres4_2stage))
+
+  expect_equal(optres_2stage[1, 5], lambdres_2stage$lambda)
+  expect_equal(optres_2stage[1, 6], ecdres1_2stage)
+  expect_equal(optres_2stage[1, 7], ecdres2_2stage)
+  expect_equal(optres_2stage[1, 8], ecdres3_2stage)
+  expect_equal(optres_2stage[1, 9], ecdres4_2stage)
+  expect_equal(optres_2stage[1, 10], meanecd_2stage)
 })
